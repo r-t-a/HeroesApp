7b1dbfae3bcc052d7641d4a5fa9a2ea4c4ad2182
Ryan <ryanandersont@gmail.com>
2015-10-07 19:36:18 -0700

    sql fix

--- app/src/main/java/com/ryan/heroestopbuilds/HeroDatabase.java
+++ app/src/main/java/com/ryan/heroestopbuilds/HeroDatabase.java
@@ -17,7 +17,7 @@ import java.util.List;
  */
 public class HeroDatabase extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "heroDatabase";
     private static final String TABLE_HEROES = "heroes";
 
