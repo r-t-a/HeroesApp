408af81177e13662ff984bc03c469bb16d6a2fc3
ScienceRobot <ryanandersont@gmail.com>
2016-05-18 15:17:43 -0700

    reverting chromie check for now

--- app/src/main/java/com/ryan/heroestopbuilds/MainActivity.java
+++ app/src/main/java/com/ryan/heroestopbuilds/MainActivity.java
@@ -255,20 +255,27 @@ public class MainActivity extends AppCompatActivity implements CallBackInterface
         ArrayList<String> finalList = new ArrayList<>();
         String lgSpacing = String.format("%" + 3 + "s", "");
         String smSpacing = String.format("%" + 1 + "s", "");
-        int weGotAChromie = 0;
         //Check first skills, this means we got a Chromie
-        for(String s : popularSkills) {
-            if (s.matches("(?i)(CompoundingAether|DeepBreath|TimewalkersPursuit|PeerIntoTheFuture)")) {
-                weGotAChromie = 1;
-                break;
+        if(popularSkills.contains("CompoundingAether") || popularSkills.contains("DeepBreath")
+                || popularSkills.contains("TimewalkersPursuit") || popularSkills.contains("PeerIntoTheFuture")) {
+
+            finalList.add("Level 1:    " + popularSkills.get(0));
+            for (int i = 1; i <= levelMod; i++) {
+                if (i <= 3) {
+                    finalList.add("Level " + (3 * i) + ": " + lgSpacing + popularSkills.get(i));
+                } else {
+                    finalList.add("Level " + (3 * i) + ": " + smSpacing + popularSkills.get(i));
+                }
             }
-        }
-        //add our final list to a new list to be passed to MainActivity
-        for (int i = 0; i <= levelMod; i++) {
-            if (i <= 3) {
-                finalList.add("Level " + (weGotAChromie + 3 * i) + ": " + lgSpacing + popularSkills.get(i));
-            } else {
-                finalList.add("Level " + (weGotAChromie + 3 * i) + ": " + smSpacing + popularSkills.get(i));
+            finalList.add("Level 19:  " + popularSkills.get(6));
+        } else {
+            //add our final list to a new list to be passed to MainActivity
+            for (int i = 0; i <= levelMod; i++) {
+                if (i <= 2) {
+                    finalList.add("Level " + (1 + 3 * i) + ": " + lgSpacing + popularSkills.get(i));
+                } else {
+                    finalList.add("Level " + (1 + 3 * i) + ": " + smSpacing + popularSkills.get(i));
+                }
             }
             finalList.add("Level 20: " + popularSkills.get(6));
         }
@@ -291,6 +298,8 @@ public class MainActivity extends AppCompatActivity implements CallBackInterface
                 .replace("G o forthe" , "Go for the")
                 .replace("Thatsthe", "Thats the")
                 .replace("Lambtothe", "Lamb to the")
+                .replace("Pastand", "Past and")
+                .replace("Reachingthrough", "Reaching Through")
                 .replace("Onthe", "On the");
     }
 
