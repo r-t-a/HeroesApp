017a26072feb07d7ce2fb598c4bf5b8b12d8c795
Ryan <ryanandersont@gmail.com>
2016-11-19 21:37:30 -0800

    hotfix

--- app/src/main/java/com/ryan/heroestopbuilds/MainActivity.java
+++ app/src/main/java/com/ryan/heroestopbuilds/MainActivity.java
@@ -148,7 +148,7 @@ public class MainActivity extends AppCompatActivity implements CallBackInterface
         CHROMIE("Chromie", R.drawable.chromie),
         DEHAKA("Dehaka", R.drawable.dehaka),
         DIABLO("Diablo", R.drawable.diablo),
-        ETC("E.T.C", R.drawable.elite_tauren_chieftain),
+        ETC("E.T.C.", R.drawable.elite_tauren_chieftain),
         FALSTAD("Falstad", R.drawable.falstad),
         GALL("Gall", R.drawable.gall),
         GAZLOWE("Gazlowe", R.drawable.gazlowe),
